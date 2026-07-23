b8835878811b4ac947be12dc4f7e163a5be8d0b2
Thilak-29 <thilakparamanathan2110@gmail.com>
2026-07-23 20:30:10 +0530

    Time: 1 ms (64.07%), Space: 43.5 MB (8.87%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        for(int j=0;j<jewels.length();j++){
+            for(int k=0;k<stones.length();k++){
+                if(jewels.charAt(j)==stones.charAt(k)){
+                    count++;
+                }
+            }
+    
+    }
+    return count;
+}
+}
