56894aa72dd0d8ad34bd8e5c1379c6a36d82a26d
Thilak-29 <thilakparamanathan2110@gmail.com>
2026-07-28 21:49:50 +0530

    Time: 8 ms (52.45%), Space: 44.1 MB (47.99%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] s1Freq = new int[26];
+        int[] windowFreq = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1Freq[s1.charAt(i) - 'a']++;
+            windowFreq[s2.charAt(i) - 'a']++;
+        }
+
+        if (matches(s1Freq, windowFreq)) {
+            return true;
+        }
+
+        for (int i = s1.length(); i < s2.length(); i++) {
+
+            windowFreq[s2.charAt(i) - 'a']++;
+
+            windowFreq[s2.charAt(i - s1.length()) - 'a']--;
+
+            if (matches(s1Freq, windowFreq)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
